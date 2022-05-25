250768b98d5f90b8bb3ed9e4ccd8647303ffe979
luka <megrelidzeluka3@gmail.com>
2022-05-26 00:34:39 +0400

    Added relevant icon api

--- src/app.js
+++ src/app.js
@@ -28,15 +28,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "daeea9cb1d13de61ced904370b898649";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?k&lat=35&lon=139&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
